8621a27d36360cdacb3f341a405c1d15bc624c5c
Kunal <vksfkcskvs@gmail.com>
2026-02-25 15:08:30 +0530

    chained middlewares and routed with static files

--- natours/app.js
+++ natours/app.js
@@ -8,8 +8,8 @@ const app = express();
 
 // 1) Middlewares
 app.use(morgan('dev'));
-
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hellio form middleware');
--- natours/server.js
+++ natours/server.js
@@ -1,5 +1,7 @@
 const app = require('./app');
 
+console.log(app.get('env'));
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`app is running on port ${port}`);
