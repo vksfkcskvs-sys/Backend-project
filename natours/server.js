2f7fa198ed2a7b239c4875206c0e26c2661be144
Kunal <vksfkcskvs@gmail.com>
2026-02-25 15:40:03 +0530

    got to know more about dotenv

--- natours/server.js
+++ natours/server.js
@@ -1,8 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-console.log(app.get('env'));
 
-const port = 3000;
+// console.log(process.env);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`app is running on port ${port}`);
 });
