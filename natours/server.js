18c9eacafe0be2208f127951c98647ace1ed0229
Kunal <vksfkcskvs@gmail.com>
2026-02-26 15:38:22 +0530

    connected mongoose with express

--- natours/server.js
+++ natours/server.js
@@ -1,11 +1,23 @@
+const dns = require('node:dns');
+dns.setServers(['1.1.1.1', '8.8.8.8']);
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
-// console.log(process.env);
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+}).then(con => {
+  console.log(con.connections);
+  console.log('DB connection successfull!');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`app is running on port ${port}`);
+  console.log(`app is running on port ${port}`);
 });
