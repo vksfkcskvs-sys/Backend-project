f3d61f66a8f4332323788af6bfce9f027cdd9ae3
Kunal <vksfkcskvs@gmail.com>
2026-02-26 17:42:43 +0530

    managed to delete tour

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -62,11 +62,27 @@ exports.updateTour = async (req, res) => {
       runValidators: true
     })
     res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated Tour Here ...>'
-    }
-  });
+      status: 'success',
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
+
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
   } catch (err) {
     res.status(400).json({
       status: 'fail',
@@ -74,13 +90,4 @@ exports.updateTour = async (req, res) => {
     });
   }
 
-  
-};
-
-exports.deleteTour = (req, res) => {
-
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
 };
