8cc05cc42fe654edaee11a6bfeb21b2be3fd25ad
Kunal <vksfkcskvs@gmail.com>
2026-03-02 15:56:43 +0530

    filtered and sorted the data

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -2,7 +2,22 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|lte|gt|lt)\b/g, match => `$${match}`)
+    console.log(JSON.parse(queryStr));
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // const tours = await Tour.find().where('duration').equals(5).where('difficulty').equals('easy');
+    if (req.query.sort) {
+      query = query.sort(req.query.sort)
+    }
+
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',
