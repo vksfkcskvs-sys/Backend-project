713aa41b8dd47b264407d45726a7a01cce3c8f56
Kunal <vksfkcskvs@gmail.com>
2026-03-02 16:35:30 +0530

    improved pagination and used aliasing feature

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -1,5 +1,12 @@
 const Tour = require('./../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+}
+
 exports.getAllTours = async (req, res) => {
   try {
     const queryObj = { ...req.query };
@@ -14,7 +21,28 @@ exports.getAllTours = async (req, res) => {
 
     // const tours = await Tour.find().where('duration').equals(5).where('difficulty').equals('easy');
     if (req.query.sort) {
-      query = query.sort(req.query.sort)
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy)
+    } else {
+      query = query.sort('-createdAt _id')
+    }
+
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields)
+    } else {
+      query = query.select('-__v')
+    }
+
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
     }
 
     const tours = await query;
