561d3a7eaf767b0b07722265f5b829219495052c
Kunal <vksfkcskvs@gmail.com>
2026-03-06 17:45:22 +0530

    started with users modelling

--- natours/routes/userRoutes.js
+++ natours/routes/userRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const userController = require('./../controllers/userController')
+const authController = require('./../controllers/authController')
 
 const Router = express.Router();
+
+Router.post('/signup', authController.signup);
 
 Router
   .route('/')
