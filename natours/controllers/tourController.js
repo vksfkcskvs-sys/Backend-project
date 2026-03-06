f57558eb459662866de61458e35059de8f4037ac
Kunal <vksfkcskvs@gmail.com>
2026-03-06 17:21:33 +0530

    completed error handling

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -1,3 +1,4 @@
+const AppError = require('../utils/appError');
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures')
 const catchAsync = require('./../utils/catchAsync')
@@ -26,8 +27,12 @@ exports.getAllTours = catchAsync(async (req, res,) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('No tour found with that id', 404))
+  }
 
   res.status(200).json({
     status: 'success',
@@ -47,11 +52,16 @@ exports.createTour = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.updateTour = catchAsync(async (req, res) => {
+exports.updateTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true
-  })
+  });
+
+  if (!tour) {
+    return next(new AppError('No tour found with that id', 404))
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -60,8 +70,12 @@ exports.updateTour = catchAsync(async (req, res) => {
   });
 });
 
-exports.deleteTour = catchAsync(async (req, res) => {
-  await Tour.findByIdAndDelete(req.params.id);
+exports.deleteTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('No tour found with that id', 404))
+  }
 
   res.status(204).json({
     status: 'success',
--- natours/server.js
+++ natours/server.js
@@ -12,6 +12,12 @@ mongoose.connect(DB).then(() => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`app is running on port ${port}`);
 });
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  server.close(() => { process.exit(1); })
+})
