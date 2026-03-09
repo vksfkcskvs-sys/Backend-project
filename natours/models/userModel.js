23c04ac4cf7fe03e1537eaead8aecf82fe7d3652
Kunal <vksfkcskvs@gmail.com>
2026-03-09 14:50:37 +0530

    installed bcrypt for hashing

--- natours/models/userModel.js
+++ natours/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -25,9 +26,19 @@ const userSchema = new mongoose.Schema({
     validate: {
       validator: function (el) {
         return el === this.password;
-      }
+      },
+      message: 'Passwords are not the same!'
     }
   }
+});
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
